88ae869b098a1917d7628400f5e4c9c00100a43f
Yash-Gupta9 <yashg9902@gmail.com>
2026-05-01 19:18:58 +0530

    first class of backend

---
+++ lec_7/module.js
@@ -0,0 +1,16 @@
+let PI = 3.14;
+
+function add(a, b) {
+    return a + b;
+}
+
+let square = (a) => {
+    return a * a;
+}
+
+// EXPORT everything
+module.exports = {
+    PI,
+    add,
+    square
+};
